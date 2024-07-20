3a59390ed35c0109517819976cde5fc742509809
ShivanshuShekhar <shivanshushekhar786@gmail.com>
2024-07-20 14:29:28 +0530

    Added Statistics & Updated Hero.jsx

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,18 +1,27 @@
 import Button from "../components/Button";
 import { arrowRight } from '../assets/icons';
+import { statistics } from "../constants";
 
 const Hero = () => {
   return (
     <section id="home" className="w-full flex zl:flex-row flex-col justify-center min-h-screen gap-10 max-container">
       <div className="relative xl:w-2/5 flex flex-col justify-center items-start w-full max-xl:padding-x pt-28">
-        <p>Our Summer Collections</p>
-        <h1>
-          <span>The New Arrivals</span>
+        <p className="text-xl font-montserrat text-coral-red">Our Summer Collections</p>
+        <h1 className="mt-10 font-palanquin text-8xl max-sm:text-[72px] max-sm:leading-[82] font-bold">
+          <span className="xl:bg-white xl:whitespace-nowrap relative z-10 pr-10">The New Arrivals</span>
           <br />
-          <span>Nike</span>Shoes
+          <span className="text-coral-red inline-block mt-3">Nike</span> Shoes
         </h1>
         <p>Discover stylish Nike arrivals, quality comfort, and innovation for your active life.</p>
         <Button label="Show Now" iconURL={arrowRight} />
+        <div className="flex justify-start items-start flex-wrap w-full mt-20 gap-16">
+          {statistics.map((stat) => (
+            <div key={stat.label}>
+              <p>{stat.value}</p>
+              <p>{stat.label}</p>
+            </div>
+          ))}
+        </div>
       </div>
     </section>
   )
